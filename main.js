ca29bb4b60caad149912f74bd77d2c4e7e2615fc
Chris <chrispdunne@gmail.com>
2019-10-21 13:19:13 +0100

    check every 60 secs, colours and wording update

--- main.js
+++ main.js
@@ -1,28 +1,57 @@
-setInterval(function(){ 
+function checkTime() {
     var date = new Date();
     var hours = date.getHours();
+    var minutes = date.getMinutes();
     var body = document.querySelector('body');
     var title = document.querySelector('.title');
+    var desc = document.querySelector('.desc');
 
-//     console.log(date);
+    // console.log(date);
+    // console.log(date.getHours());
+    // console.log(date.getMinutes());
+    // console.log('running');
 
-// console.log(date.getHours());
-// console.log(date.getMinutes());
+    if (hours < 9 || (hours === 9 && minutes <= 30)) {
+        //early!
+        body.className = 'early';
+        title.innerHTML = 'Early bird';
+        desc.innerHTML = '';
 
-console.log('running');
-
-if (hours < 7) {
-    //early!
-    body.className = 'early';
-    title.innerHTML = 'Early bird'
-} else if (hours >= 7 && hours < 11) {
-    // email / social / pr
-    body.className = 'emails';
-    title.innerHTML = 'Emails / Social / PR'
-} else if (hours >= 11) {
-    // client work
-    body.className = 'projects';
-    title.innerHTML = 'Project work'
+    } else if (hours === 9) {
+        // email / social / pr
+        body.className = 'emails';
+        title.innerHTML = 'Setup';
+        desc.innerHTML = 'Emails, ASANA, setup for the day';
+    } else if (hours === 10) {
+        // email / social / pr
+        body.className = 'emails';
+        title.innerHTML = 'PR '
+        desc.innerHTML = 'Social / PR work for sales funnel';
+    } else if (hours >= 11 && hours < 15) {
+        // client work
+        body.className = 'projects';
+        title.innerHTML = 'Project work';
+        desc.innerHTML = 'Project/client work';
+    } else if (hours === 15 && minutes <= 30) {
+        // client work
+        body.className = 'emails';
+        title.innerHTML = 'Emails';
+        desc.innerHTML = 'Afternoon email catchup';
+    } else if (hours >= 15 && hours < 19) {
+        // client work
+        body.className = 'projects';
+        title.innerHTML = 'Project work';
+        desc.innerHTML = 'Project/client work';
+    } else {
+        body.className = 'early';
+        title.innerHTML = 'END';
+        desc.innerHTML = '';
+    }
 }
+checkTime();
+setInterval(checkTime, 60000);
 
-}, 2000);
+
+
+// 9.30 - 10 setup, asana, emails
+// 10 - 11, social PR
