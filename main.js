8a5eb621a64b12e8e58b7e6796dee2de4d4d6a82
Chris <chrispdunne@gmail.com>
2019-10-22 13:13:49 +0100

    log

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var deferredPrompt;
 window.addEventListener('beforeinstallprompt', (e) => {
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
+    console.log({deferredPrompt});
+    
 });
 
 var install = document.querySelector('.install');
