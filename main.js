9dabf904c15242cbdcce5fb44b97e45c6104c098
Chris <chrispdunne@gmail.com>
2019-10-22 12:41:35 +0100

    pwa bare min

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+// PWA
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+});
+
+var install = document.querySelector('.install');
+install.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    install.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+        .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+            } else {
+            console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+});
+
+
+
+// Logic
 var status = 0;
 var airhorn = document.querySelector('.airhorn');
 var promise = airhorn.play();
