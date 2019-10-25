0439e4ad9beef547ce20f450b1c852b7be846a0b
Chris <chrispdunne@gmail.com>
2019-10-25 12:43:47 +0100

    update sw

--- sw.js
+++ sw.js
@@ -2,6 +2,8 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('sw-cache').then(function(cache) {
       return cache.add('index.html');
+      console.log({navigator});
+      
     })
   );
 });
